1540c7e3aa7fa988416b4e260369eacb7e04dae6
Anirban Aditya Halder <71653026+banboy38@users.noreply.github.com>
2020-10-12 19:14:56 +0530

    Update squareprog.py

--- squareprog.py
+++ squareprog.py
@@ -9,8 +9,9 @@ for x in range(0, cases):
     q = int(input())
     l1.append(q)
     flag = 0
+    if(q < 0):
+        q = -q
 
-    if(q>0):
         for y in range(q, 1, -1):
             for z in range(q-1, 1, -1):
                 w = math.pow(y, 2) - math.pow(z, 2)
@@ -26,23 +27,9 @@ for x in range(0, cases):
                 break
         if(flag == 0):
             l2.append(-1)
-    elif(q<0):
-        for y in range(q, -1, 1):
-            for z in range(q+1, -1, 1):
-                w = math.pow(y, 2) - math.pow(z, 2)
-                if(w == q):
-                    l2.append(-y)
-                    l2.append(-z)
-                    flag = 1
-                    break
             
-                if(flag == 1):
-                    break
-            if(flag == 1):
-                break
-        if(flag == 0):
-            l2.append(-1)
-    
+            
+        
 for x in range(0, len(l2), 2):
     if(l2[x] != -1):
         print(l2[x], end = " ")
